714387d013c4c2b0edea3a2d88204a996f62dfa3
Tanawad Payom <tanawad47@gmail.com>
2021-09-24 13:54:05 +0700

    ly

--- function.js
+++ function.js
@@ -1,34 +1,34 @@
 
-            alert("ยินดีต้อนรับ");
-            function hello (){
-                alert("สวัสดีครับ!! ท่านผู้เจริญ");
-            }
-            function blue(){
-                var x = document.querySelectorAll("li");
-                var i;
-                for (i = 0; i < x.length; i++) {
-                    x[i].style.color = "blue";
-                }
-            }
-            function red(){
-                var x = document.querySelectorAll("li");
-                var i;
-                for (i = 0; i < x.length; i++) {
-                    x[i].style.color = "red";
-                }
-            }
-            function white(){
-                var x = document.querySelectorAll("li");
-                var i;
-                for (i = 0; i < x.length; i++) {
-                    x[i].style.color = "white";
-                }
-            }
-            function black(){
-                var x = document.querySelectorAll("li");
-                var i;
-                for (i = 0; i < x.length; i++) {
-                    x[i].style.color="black";
-                }
-            }
+alert("ยินดีต้อนรับ");
+function hello (){
+    alert("สวัสดีครับ!! ท่านผู้เจริญ");
+}
+function blue(){
+    var x = document.querySelectorAll("li");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "blue";
+    }
+}
+function red(){
+    var x = document.querySelectorAll("li");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "red";
+    }
+}
+function white(){
+    var x = document.querySelectorAll("li");
+    var i;
+        for (i = 0; i < x.length; i++) {
+            x[i].style.color = "white";
+    }
+}
+function black(){
+    var x = document.querySelectorAll("li");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color="black";
+    }
+}
         
